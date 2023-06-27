3b33d139d7b18111e80fa87f7319a08b8bd7e95b
fastboy <giagiang200@gmail.com>
2023-06-27 11:11:16 +0700

    addNavbar+buttonchatbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
+import ChatRoom from "./pages/ChatRoom";
 function App() {
   return (
-   <div> 
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button className= "btn">Button</button>
-   </div>
-  )
+    <div>
+      <Navbar />
+      {/* <Login /> */}
+      <ChatRoom />
+    </div>
+  );
 }
 
-export default App
- 
+export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,17 @@
+const Login = () => {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello there 👋</h1>
+          <p className="py-6">
+          Join the conversation, meet new people, and make connections in on shared room 
+          </p>
+          <button className="btn "> login with google </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
